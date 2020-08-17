f0f77b594e4c2f5649b8b96d93d8ef0ce56cba5b
Mark Coatsworth <coatsworth@cs.wisc.edu>
2020-08-17 14:10:38 -0500

    Cleaned up syntax per flake8

--- gwdata_plugin.py
+++ gwdata_plugin.py
@@ -3,33 +3,27 @@
 import argparse
 import classad
 import gwdatafind
-import io
-import json
-import os
 import pathlib
-import posixpath
 import pycurl
-import shutil
-import socket
 import sys
 import time
-
-from urllib.parse import urlparse
 
 DEFAULT_TIMEOUT = 30
 GWDATA_PLUGIN_VERSION = '1.0.0'
 
-def print_help(stream = sys.stderr):
+
+def print_help(stream=sys.stderr):
     help_msg = '''Usage: {0} -infile <input-filename> -outfile <output-filename>
        {0} -classad
 
 Options:
-  -classad                    Print a ClassAd containing the capablities of this
-                              file transfer plugin.
+  -classad                    Print a ClassAd containing the capablities of
+                              this file transfer plugin.
   -infile <input-filename>    Input ClassAd file
   -outfile <output-filename>  Output ClassAd file
 '''
     stream.write(help_msg.format(sys.argv[0]))
+
 
 def print_capabilities():
     capabilities = {
@@ -39,6 +33,7 @@ def print_capabilities():
          'Version': GWDATA_PLUGIN_VERSION,
     }
     sys.stdout.write(classad.ClassAd(capabilities).printOld())
+
 
 def parse_args():
 
@@ -53,11 +48,14 @@ def parse_args():
 
     parser = argparse.ArgumentParser()
     parser.add_argument("-classad", action="store_true",
-        help="Output classad of options supported by this transfer plugin")
+                        help="Output classad of options supported by this "
+                        "transfer plugin")
     parser.add_argument("-infile", type=str, action="store",
-        help="Input file of classads describing files to transfer")
+                        help="Input file of classads describing files to "
+                        "transfer")
     parser.add_argument("-outfile", type=str, action="store",
-        help="Output file of classads describing file transfer results")
+                        help="Output file of classads describing file "
+                        "transfer results")
     args = parser.parse_args()
 
     # If -classad, print the capabilities of the plugin and exit early
@@ -67,10 +65,12 @@ def parse_args():
 
     return {'infile': args.infile, 'outfile': args.outfile}
 
+
 def format_error(error):
     return '{0}: {1}'.format(type(error).__name__, str(error))
 
-def get_error_dict(error, url = ''):
+
+def get_error_dict(error, url=''):
     error_string = format_error(error)
     error_dict = {
         'TransferSuccess': False,
@@ -79,6 +79,7 @@ def get_error_dict(error, url = ''):
     }
 
     return error_dict
+
 
 class GWDataPlugin:
 
@@ -92,17 +93,24 @@ class GWDataPlugin:
         # Parse the input arguments
         for arg in args:
             attr, value = arg.split("=")
-            if attr == "observatory": observatory = value
-            if attr == "type": type = value
-            if attr == "s": start_frame = value
-            if attr == "e": end_frame = value
+            if attr == "observatory":
+                observatory = value
+            if attr == "type":
+                type = value
+            if attr == "s":
+                start_frame = value
+            if attr == "e":
+                end_frame = value
 
         # If any input arguments are missing, report error and exit
         try:
             observatory, type, end_frame, start_frame
         except NameError:
             with open(self.outfile, 'w') as outfile:
-                outfile_dict = get_error_dict(str("gwdata:// urls must include arguments 'observatory', 'type', 's' (start frame), 'e' (end frame)"))
+                outfile_dict = get_error_dict(
+                    str("gwdata:// urls must include arguments 'observatory', "
+                        "'type', 's' (start frame), 'e' (end frame)")
+                )
                 outfile.write(str(classad.ClassAd(outfile_dict)))
                 sys.exit(-1)
 
@@ -116,7 +124,9 @@ class GWDataPlugin:
                 gpsend=int(end_frame)
             )
         except Exception as e:
-            print("Error retrieving gwdatafind URLs: " + str(sys.exc_info()[0]) + ": " + str(e))
+            print("Error retrieving gwdatafind URLs: {} ({})".format(
+                str(sys.exc_info()[0]), str(e)
+            ))
 
         return urls
 
@@ -127,9 +137,12 @@ class GWDataPlugin:
         # Parse the input arguments
         for arg in args:
             attr, value = arg.split("=")
-            if attr == "e": end_frame = int(value)
-            if attr == "cache": cache = value
-            if attr == "metadata_file": metadata_filename = value
+            if attr == "e":
+                end_frame = int(value)
+            if attr == "cache":
+                cache = value
+            if attr == "metadata_file":
+                metadata_filename = value
 
         metadata_file = open(metadata_filename, "w")
 
@@ -151,7 +164,7 @@ class GWDataPlugin:
                 file_name = url.split("/")[-1]
                 metadata_tokens = file_name.split("-")
 
-                # If this is the first url in the list, set a couple gps tracking variables
+                # If this is the first url in the list, set tracking variables
                 if url == urls[0]:
                     gps_start = int(metadata_tokens[2])
                     gps_counter = gps_start
@@ -178,15 +191,18 @@ class GWDataPlugin:
                         gps_start = gps_current
                         gps_counter = gps_start
         else:
-            metadata_file.write("Error: {0} is not a valid cache type. Allowed types are \"lal\", \"lal-cache\", \"frame\" and \"frame-cache\".\n".format(cache))
+            metadata_file.write(
+                "Error: {0} is not a valid cache type. Allowed types are "
+                "\"lal\", \"lal-cache\", \"frame\" and \"frame-cache\".\n"
+                .format(cache)
+            )
 
         metadata_file.close()
-
 
     def download_data(self, url):
 
         transfer_stats = []
-        
+
         gwdatafind_server = url[(url.find("://") + 3):url.find("?")]
         gwdata_args = url[(url.find("?") + 1):].split("&")
 
@@ -247,11 +263,9 @@ class GWDataPlugin:
         if "cache" in url:
             self.create_cache(gwdata_args, gwdata_urls)
 
-
         # Looks like everything downloaded correctly. Exit success.
         curl.close()
         return transfer_stats, True
-
 
 
 if __name__ == '__main__':
@@ -264,7 +278,7 @@ if __name__ == '__main__':
 
     gwdata = GWDataPlugin(args['outfile'])
 
-    # Parse in the classads stored in the input file. 
+    # Parse in the classads stored in the input file.
     # Each ad represents a single file to be transferred.
     try:
         infile_ads = classad.parseAds(open(args['infile'], 'r'))
@@ -276,13 +290,15 @@ if __name__ == '__main__':
         except Exception:
             pass
         sys.exit(-1)
-    
+
     # Now iterate over the list of classads and perform the transfers.
     try:
         with open(args['outfile'], 'w') as outfile:
             for ad in infile_ads:
                 try:
-                    result_ads, transfer_success = gwdata.download_data(ad['Url'])
+                    result_ads, transfer_success = gwdata.download_data(
+                        ad['Url']
+                    )
                     # Output the results to file
                     for ad in result_ads:
                         outfile.write(str(classad.ClassAd(ad)))
@@ -293,7 +309,7 @@ if __name__ == '__main__':
                 except Exception as err:
                     print(err)
                     try:
-                        outfile_dict = get_error_dict(err, url = ad['Url'])
+                        outfile_dict = get_error_dict(err, url=ad['Url'])
                         outfile.write(str(classad.ClassAd(outfile_dict)))
                     except Exception:
                         pass
